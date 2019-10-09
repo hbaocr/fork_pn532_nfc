c72c02e1091fce825d87417e70b31f358e885c9e
Duong Huynh Bao <duonghuynhbaocr@gmail.com>
2019-10-09 23:30:51 +0700

    add testwrite

--- index.js
+++ index.js
@@ -25,12 +25,26 @@ rfid.on('ready', async function() {
 });
 rfid.on('tag', async function(tag_info) {
     if(last_uuid===tag_info.uid){
-       //console.log('tag already checked')
+      
     }else{
         last_uuid=tag_info.uid;
-        console.log('new uuid = ',tag_info.uid);
+        console.log(Date.now(),'  new uuid = ',tag_info.uid);
+
+        let messages = [
+            ndef.uriRecord('http://www.google.com/qwertyuiopasdsfghjhgfdsfghgdfstdrghkjlkjhkjl'),
+            ndef.textRecord('12345678901234567890123456789012345678901234567890abcdefighklmn')
+        ];
+        let data = ndef.encodeMessage(messages);
+        console.log('write_data_byte: ',data.length);
+
+        let res= await rfid.writeNdefData(data);
+        console.log(res);
+
+        let fr= await rfid.InRelease();
+        log_cmd_ret_hex(fr,'Tag release');
     }
-   
+    
+  
     rfid.ScanTagWithEvent();//scan next tag
 });
 rfid.on('tag_err', async function(tag_info) {
@@ -39,75 +53,3 @@ rfid.on('tag_err', async function(tag_info) {
 });
 
 
-// rfid.on('ready', async function () {
-
-//     console.log('Waiting for a tag...');
-    
-//     // rfid.getFirmwareVersion().then(fr=>{
-//     //     console.log(fr);
-//     // })
-//     rfid.sendCommand([0x02]).then(fr=>{
-//         console.log('version: ',fr);
-//     })
-//     // rfid.scanTag().then(function(tag) {
-//     //     console.log('Tag found:', tag);
-
-//     //     var messages = [
-//     //         ndef.uriRecord('http://www.google.com'),
-//     //         ndef.textRecord('test')
-//     //     ];
-//     //     var data = ndef.encodeMessage(messages);
-
-//     //     console.log('Writing tag data...');
-//     //     rfid.writeNdefData(data).then(function(response) {
-//     //         console.log('Write successful');
-//     //     });
-//     // });
-// });
-
-
-
-// // let cmd_imDeselect=()=>{
-// //    	//PN532 User Manual-UM0701-02
-// // 	//7.3.10 InDeselect
-// // 	/**
-// // 	 * D4 44 Tg
-// // 	 * Tg  a byte containing the logical number of the relevant target (=0x00 : deselect all tag )
-// // 	 */
-// //     const pn532_deslect_cmd_payload = [0x44, 0x01];
-// //     return Buffer.from([...pn532_deslect_cmd_payload])
-// // }
-
-
-
-// // try {
-// //     let a =   rfid.sendCommand([0x02]).then((frame) => {
-// //         var body = frame.getDataBody();
-      
-// //     }).catch(e=>{
-// //         console.log(e)
-// //     });
-// // } catch (error) {
-    
-// // }
-
-
-
-// // rfid.on('ready', async function() {
-
-// //     console.log('Listening for a tag scan...');
-// //     console.log(cmd_imDeselect());
-   
-// //     rfid.on('tag', function(tag) {
-// //         console.log('Tag', tag.uid);
-// //         let indeselct=cmd_imDeselect();
-// //         //rfid.sendCommand(indeselct);
-// //        // console.log('Reading tag data...');
-// //         // rfid.readNdefData().then(function(data) {
-// //         //     console.log('Tag data:', data);
-
-// //         //     var records = ndef.decodeMessage(Array.from(data));
-// //         //     console.log(records);
-// //         // });
-// //     });
-// // });
