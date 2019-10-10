8f74637bf6fb1dc1fdb42b19cfc5d6b1a1efc9c6
Duong Huynh Bao <duonghuynhbaocr@gmail.com>
2019-10-10 09:11:05 +0700

    add cardout event

--- index.js
+++ index.js
@@ -2,30 +2,36 @@
 
 const pn532 = require('pn532');
 const SerialPort = require('serialport');
-const serialPort = new SerialPort('/dev/cu.usbserial-AL00FN6Z', { baudRate: 115200, autoOpen: true});
+const serialPort = new SerialPort('/dev/cu.usbserial-AL00FN6Z', { baudRate: 115200});
+
 const option={
-    pollInterval:0 /**Disable software auto scantag */
+    pollInterval:0 /**Disable software auto scanning tag */
 }
 const rfid = new pn532.PN532(serialPort,option);
 const ndef = require('ndef');
 
-const cmd_code = require("./pn532_cmd");
 
-function log_cmd_ret_hex(fr,label='log_data : '){
-     let dat=fr.getData().toString('hex').match(/.{1,2}/g).join(':');
+let uuid = new Set();
+let last_uuid='';
+let hdl_timeout=0;
+function log_cmd_ret_hex(buff,label='log_data : '){
+     let dat=buff.toString('hex').match(/.{1,2}/g).join(':');
      console.log(label,dat);
 }
 
 
-let uuid = new Set();
-let last_uuid='';
+
 console.log('Waiting for rfid ready event...');
 rfid.on('ready', async function() {
     rfid.ScanTagWithEvent();//first scan
 });
 rfid.eventScanTag.on('tag', async function(tag_info) {
     if(last_uuid===tag_info.uid){
-      
+        clearTimeout(hdl_timeout);
+        hdl_timeout= setTimeout(()=>{
+            console.log('card.out event');
+            last_uuid='';
+          },300);  
     }else{
         last_uuid=tag_info.uid;
         console.log(Date.now(),'  new uuid = ',tag_info.uid);
